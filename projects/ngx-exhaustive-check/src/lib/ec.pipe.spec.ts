dc5c13661bb64ea99ac8b74b9691dfaa10ff1720
Ernest <>
2024-06-21 22:34:20 +0800

    test: add tests

--- projects/ngx-exhaustive-check/src/lib/ec.pipe.spec.ts
+++ projects/ngx-exhaustive-check/src/lib/ec.pipe.spec.ts
@@ -5,4 +5,50 @@ describe('EcPipe', () => {
     const pipe = new EcPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('works with never', () => {
+    const pipe = new EcPipe();
+    const value = undefined as never;
+
+    const result = pipe.transform(value);
+
+    expect(result).toBe(value);
+  });
+
+  it('works with undefined', () => {
+    const pipe = new EcPipe();
+    const value = undefined;
+
+    const result = pipe.transform(value, undefined);
+
+    expect(result).toBe(value);
+  });
+
+  it('works with null', () => {
+    const pipe = new EcPipe();
+    const value = null;
+
+    const result = pipe.transform(value, null);
+
+    expect(result).toBe(value);
+  });
+
+  it('works with unions', () => {
+    const pipe = new EcPipe();
+    const value = null as undefined | null;
+
+    const result = pipe.transform(value, undefined, null);
+
+    expect(result).toBe(value);
+  });
+
+  it('should fail when parameters are missing', () => {
+    const pipe = new EcPipe();
+    const value = null as undefined | null;
+
+    // @ts-expect-error
+    const result = pipe.transform(value, undefined);
+
+    expect(result).toBe(value);
+  });
 });

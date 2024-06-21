58d37cd5ef1d05b5216d0695d84e5afc23657326
Ernest <>
2024-06-21 21:25:26 +0800

    feat: const-like inference

--- README.md
+++ README.md
@@ -57,6 +57,9 @@ export class AppComponent {
 
 ## Advanced Usage
 
+> The following example only works with `typescript@^5.0.0`.
+> See [const-type-parameters](https://devblogs.microsoft.com/typescript/announcing-typescript-5-0-beta/#const-type-parameters#const-type-parameters) for more details.
+
 Compile successfully:
 
 ```ts
@@ -65,6 +68,37 @@ import { EcPipe } from "ngx-exhaustive-check";
 enum Answer {
   Yes,
   No,
+  Maybe,
+}
+
+@Component({
+  imports: [EcPipe],
+  // ..
+})
+export class AppComponent {
+  answer: Answer = Answer.Yes;
+  Answer = Answer;
+}
+```
+
+```
+@switch (answer) {
+  @case (Answer.Yes) {}
+  @default {
+    <div>{{ answer | ec: Answer.No : Answer.Maybe }}</div>
+  }
+}
+```
+
+Compile failed:
+
+```ts
+import { EcPipe } from "ngx-exhaustive-check";
+
+enum Answer {
+  Yes,
+  No,
+  Maybe,
 }
 
 @Component({
@@ -82,36 +116,7 @@ export class AppComponent {
   @case (Answer.Yes) {}
   @default {
     <div>{{ answer | ec: Answer.No }}</div>
-  }
-}
-```
-
-Compile failed:
-
-```ts
-import { EcPipe } from "ngx-exhaustive-check";
-
-enum Answer {
-  Yes,
-  No,
-}
-
-@Component({
-  imports: [EcPipe],
-  // ..
-})
-export class AppComponent {
-  answer: Answer = Answer.Yes;
-  Answer = Answer;
-}
-```
-
-```
-@switch (answer) {
-  @case (Answer.Yes) {}
-  @default {
-    <div>{{ answer | ec }}</div>
-       <!-- ^^^^^^ Argument of type 'Answer' is not assignable to parameter of type 'never'.ngtsc(2345) -->
+       <!-- ^^^^^^ Argument of type 'Answer' is not assignable to parameter of type 'never'. Type 'Answer' is not assignable to type 'never'.ngtsc(2345) -->
   }
 }
 ```
--- projects/ngx-exhaustive-check/src/lib/ec.pipe.ts
+++ projects/ngx-exhaustive-check/src/lib/ec.pipe.ts
@@ -6,10 +6,151 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: true,
 })
 export class EcPipe implements PipeTransform {
-  transform<T, U = never>(
-    value: T extends U ? (U extends T ? T : never) : never,
-    _expected: U = undefined as U,
-  ): never {
+  transform(value: never): never;
+  transform<T, const U1>(
+    value: TypeEqual<T, U1> extends true ? T : never,
+    _expected1: U1,
+  ): never;
+  transform<T, const U1, const U2>(
+    value: TypeEqual<T, U1 | U2> extends true ? T : never,
+    _expected1: U1,
+    _expected2: U2,
+  ): never;
+  transform<T, const U1, const U2, const U3>(
+    value: TypeEqual<T, U1 | U2 | U3> extends true ? T : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+  ): never;
+  transform<T, const U1, const U2, const U3, const U4>(
+    value: TypeEqual<T, U1 | U2 | U3 | U4> extends true ? T : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+  ): never;
+  transform<T, const U1, const U2, const U3, const U4, const U5>(
+    value: TypeEqual<T, U1 | U2 | U3 | U4 | U5> extends true ? T : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+    _expected5: U5,
+  ): never;
+  transform<T, const U1, const U2, const U3, const U4, const U5, const U6>(
+    value: TypeEqual<T, U1 | U2 | U3 | U4 | U5 | U6> extends true ? T : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+    _expected5: U5,
+    _expected6: U6,
+  ): never;
+  transform<
+    T,
+    const U1,
+    const U2,
+    const U3,
+    const U4,
+    const U5,
+    const U6,
+    const U7,
+  >(
+    value: TypeEqual<T, U1 | U2 | U3 | U4 | U5 | U6 | U7> extends true
+      ? T
+      : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+    _expected5: U5,
+    _expected6: U6,
+    _expected7: U7,
+  ): never;
+  transform<
+    T,
+    const U1,
+    const U2,
+    const U3,
+    const U4,
+    const U5,
+    const U6,
+    const U7,
+    const U8,
+  >(
+    value: TypeEqual<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8> extends true
+      ? T
+      : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+    _expected5: U5,
+    _expected6: U6,
+    _expected7: U7,
+    _expected8: U8,
+  ): never;
+  transform<
+    T,
+    const U1,
+    const U2,
+    const U3,
+    const U4,
+    const U5,
+    const U6,
+    const U7,
+    const U8,
+    const U9,
+  >(
+    value: TypeEqual<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8 | U9> extends true
+      ? T
+      : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+    _expected5: U5,
+    _expected6: U6,
+    _expected7: U7,
+    _expected8: U8,
+    _expected9: U9,
+  ): never;
+  transform<
+    T,
+    const U1,
+    const U2,
+    const U3,
+    const U4,
+    const U5,
+    const U6,
+    const U7,
+    const U8,
+    const U9,
+    const U10,
+  >(
+    value: TypeEqual<
+      T,
+      U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8 | U9 | U10
+    > extends true
+      ? T
+      : never,
+    _expected1: U1,
+    _expected2: U2,
+    _expected3: U3,
+    _expected4: U4,
+    _expected5: U5,
+    _expected6: U6,
+    _expected7: U7,
+    _expected8: U8,
+    _expected9: U9,
+    _expected10: U10,
+  ): never;
+  transform(value: unknown, ..._: unknown[]) {
     return value as never;
   }
 }
+
+type TypeEqual<Actual, Expect> =
+  (<T>() => T extends Actual ? 1 : 2) extends <T>() => T extends Expect ? 1 : 2
+    ? true
+    : false;
